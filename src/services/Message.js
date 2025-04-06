0b7b512aa40f924804dd13fbdce0bc5ed2ec0222
LuiferEduardoo <luifer01ortegaperez@gmail.com>
2025-04-05 20:01:46 -0500

    feat: sort by shipping date

--- src/services/Message.js
+++ src/services/Message.js
@@ -1,23 +1,32 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const MessageModel = require('../models/Message');
+const MessageModel = require("../models/Message");
 
-class Message { 
+class Message {
   async getMessagesByUserId(req, res) {
     try {
-      const messages = await MessageModel.find({ receiverId: req.params.id, senderId: req.user.sub  }).populate('senderId', 'name');
+      const messages = await MessageModel.find({
+        receiverId: req.params.id,
+        senderId: req.user.sub,
+      })
+      .sort({ createdAt: -1 })
+      .populate("senderId", "name");
       return res.status(200).json(messages);
     } catch (error) {
-      return res.status(500).json({ error: 'Error interno del servidor' });
+      return res.status(500).json({ error: "Error interno del servidor" });
     }
   }
 
   async getMessagesByGroupId(req, res) {
     try {
-      const messages = await MessageModel.find({ groupId: req.params.id, senderId: req.user.sub }).populate('senderId', 'name');
+      const messages = await MessageModel.find({
+        groupId: req.params.id,
+      })
+      .sort({ createdAt: -1 })
+      .populate("senderId", "name picture");
       return res.status(200).json(messages);
     } catch (error) {
-      return res.status(500).json({ error: 'Error interno del servidor' });
+      return res.status(500).json({ error: "Error interno del servidor" });
     }
   }
 
@@ -26,7 +35,7 @@ class Message {
       if (!mongoose.Types.ObjectId.isValid(receiverId)) {
         throw new Error("ID de receptor inválido");
       }
-  
+
       const pipeline = [
         { $match: { receiverId: new mongoose.Types.ObjectId(receiverId) } },
         { $sort: { timestamp: -1 } }, // Ordenamos primero por timestamp en orden descendente
@@ -34,16 +43,16 @@ class Message {
           $group: {
             _id: "$senderId",
             lastMessage: { $first: "$$ROOT" }, // Tomamos el mensaje más reciente de cada sender
-            messageCount: { $sum: 1 }
-          }
+            messageCount: { $sum: 1 },
+          },
         },
         {
           $lookup: {
             from: "users", // Asegurar que coincide con el nombre de la colección en MongoDB
             localField: "_id",
             foreignField: "_id",
-            as: "sender"
-          }
+            as: "sender",
+          },
         },
         { $unwind: "$sender" }, // Convertimos el array de `sender` en objeto
         {
@@ -56,21 +65,20 @@ class Message {
             lastMessage: {
               id: "$lastMessage._id",
               content: "$lastMessage.message",
-              timestamp: "$lastMessage.timestamp"
+              timestamp: "$lastMessage.timestamp",
             },
-            messageCount: 1
-          }
+            messageCount: 1,
+          },
         },
-        { $sort: { "lastMessage.timestamp": -1 } } // Ordenamos por el último mensaje de cada usuario
+        { $sort: { "lastMessage.timestamp": -1 } }, // Ordenamos por el último mensaje de cada usuario
       ];
-  
+
       const result = await MessageModel.aggregate(pipeline);
       return res.status(200).json(result);
     } catch (error) {
-      return res.status(500).json({ error: 'Error interno del servidor' });
+      return res.status(500).json({ error: "Error interno del servidor" });
     }
   }
-  
 }
 
 module.exports = Message;
